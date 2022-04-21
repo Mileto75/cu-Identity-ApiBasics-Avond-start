2842f7a62110953df0bddb3ad958f064446d8ad8
Mileto Di Marco <mileto.di.marco@howest.be>
2022-04-21 16:13:55 +0200

    Add project files.

---
+++ cu.ApiBasics.Lesvoorbeeld.Avond.Api/Dtos/Account/AccountLoginRequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cu.ApiBasics.Lesvoorbeeld.Avond.Api.Dtos.Account
+{
+    public class AccountLoginRequestDto
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
